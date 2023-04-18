10421d848965bde5a3e9178de288daa8fc227e61
runninginair <runninginair@gmail.com>
2023-04-17 21:28:43 -0700

    Updated front API url for Heroku.com

--- src/main/java/com/example/controller/ActivityController.java
+++ src/main/java/com/example/controller/ActivityController.java
@@ -4,15 +4,23 @@ import com.example.model.User;
 import com.example.services.ActivityService;
 import com.example.model.Activity;
 import com.example.services.UserService;
+
 import java.sql.Date;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ActivityController {
 
-    public static final String PLUS1_FRONTEND_API = "http://localhost:3000";
+    /** For local frontend app */
+    // public static final String PLUS1_FRONTEND_API = "http://localhost:3000";
+
+    /**
+     * For remote (Heroku) frontend app
+     */
+    public static final String PLUS1_FRONTEND_API = "https://plusone-frontend.herokuapp.com";
 
     @Autowired
     ActivityService activityService = new ActivityService();
@@ -23,7 +31,7 @@ public class ActivityController {
 
     @CrossOrigin(origins = PLUS1_FRONTEND_API)
     @GetMapping("/activities/{uid}/date/{date}")
-    public List<Activity> getActivitiesByUserIdAndDate(@PathVariable("uid") long uid, @PathVariable("date") String date){
+    public List<Activity> getActivitiesByUserIdAndDate(@PathVariable("uid") long uid, @PathVariable("date") String date) {
         System.out.println("uid = " + uid + ", date = " + date);
         List<Activity> activities = activityService.getActivitiesByDay(uid, Date.valueOf(date));
         return activities;
@@ -31,13 +39,14 @@ public class ActivityController {
 
     /**
      * create a new activity by userid, date, activity name
+     *
      * @param uid
      * @param dateString
      * @param activityName
      */
     @CrossOrigin(origins = PLUS1_FRONTEND_API)
     @PostMapping("/activity/create/{userid}/date/{date}/name/{name}")
-    public boolean createActivity(@PathVariable("userid") long uid, @PathVariable("date") String dateString, @PathVariable("name") String activityName){
+    public boolean createActivity(@PathVariable("userid") long uid, @PathVariable("date") String dateString, @PathVariable("name") String activityName) {
 //        System.out.println("uid = " + uid + ", date = " + date);
         User user = userService.getUserById(uid);
         if (user == null) {
@@ -56,14 +65,14 @@ public class ActivityController {
 
     @CrossOrigin(origins = PLUS1_FRONTEND_API)
     @GetMapping("/activity/getDistanceByTimeRange/{userId}/{start}/{end}")
-    public double getDistanceByTimeRange(@PathVariable("userId")Long userId, @PathVariable("start")Date start, @PathVariable("end")Date end){
-        return activityService.getDistanceByTimeRange(userId,start,end);
+    public double getDistanceByTimeRange(@PathVariable("userId") Long userId, @PathVariable("start") Date start, @PathVariable("end") Date end) {
+        return activityService.getDistanceByTimeRange(userId, start, end);
     }
 
     @CrossOrigin(origins = PLUS1_FRONTEND_API)
     @GetMapping("/activity/getCaloriesByTimeRange/{userId}/{start}/{end}")
-    public double getCaloriesByTimeRange(@PathVariable("userId")Long userId, @PathVariable("start")Date start, @PathVariable("end")Date end){
-        return activityService.getCaloriesByTimeRange(userId,start,end);
+    public double getCaloriesByTimeRange(@PathVariable("userId") Long userId, @PathVariable("start") Date start, @PathVariable("end") Date end) {
+        return activityService.getCaloriesByTimeRange(userId, start, end);
     }
 
 }
